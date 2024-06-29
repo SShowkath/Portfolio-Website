7941b2472c65e5931f53201e9a562a3f7272f142
Shahrukh Showkath <shahrukhshowkath@gmail.com>
2024-06-29 10:39:50 -0500

    New site backbone - overhauled

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,23 +1,15 @@
-import React from "react"
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { useState } from 'react'
 import './App.css'
-import Landing from "./pages/Landing"
-import Experience from "./pages/Experience"
-import Contact from "./pages/Contact"
 
 function App() {
+  
 
   return (
     <>
-      <BrowserRouter>
-        
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/Home" element={<Landing />} />
-          <Route path="/Experience" element={<Experience/>} />
-          <Route path="/Contact" element={<Contact/>} />
-        </Routes>
-      </BrowserRouter>
+      <section>hero</section>
+      <section>parallax</section>
+      <section>Portfolio</section>
+      <section>Parallax</section>
     </>
   )
 }
