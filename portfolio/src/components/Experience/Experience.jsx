bb5c31053cb11fe7fd6ac38cbab6eaf609f08e20
Shahrukh Showkath <shahrukhshowkath@gmail.com>
2024-07-05 17:51:58 -0500

    rewrote image filepaths

--- portfolio/src/components/Experience/Experience.jsx
+++ portfolio/src/components/Experience/Experience.jsx
@@ -8,50 +8,50 @@ export default function Experience() {
           title: 'JP Morgan Chase',
           date: 'Summer 2024',
           description: 'Returned to JPMC as a SWE intern for Financial Data Integration team for Corporate Technology',
-          img: "./public/JP_Morgan_logo.png"
+          img: "./JP_Morgan_logo.png"
         },
         {
           title: 'DesignAI Group',
           date: 'Jan - May 2024',
           description: 'Research assistant for Dr. Qian Yang\'s group, where I worked on a RAG pipeline for a medical literature assistant that personalizes research summaries for users',
-          img: "./public/cornell.png"
+          img: "./cornell.png"
         },
         {
             title: 'Cornell University',
             date: 'Jan - Dec 2024 (Expected)',
             description: 'Continued education to pursue a Masters of Engineering in Computer Science at Cornell Univeristy.',
-            img: "./public/cornell.png"
+            img: "./cornell.png"
           },
         {
             title: 'University of Texas at Dallas',
             date: '2021 - Fall 2023',
             description: 'Graduated with a B.S. in Computer Science',
-            img: "./public/utd.jpg"
+            img: "./utd.jpg"
           },
         {
           title: 'JP Morgan Chase',
           date: 'Summer 2023',
           description: "SWE intern on the Global Banking Platform team, where I designed and built a TerraForm lifecycle management application to streamline infrastructure provisioning.",
-          img: "./public/JP_Morgan_logo.png"
+          img: "./JP_Morgan_logo.png"
         },
         {
             title: 'ACM UT Dallas',
             date: 'Jan - May 2023',
             description: 'Conducted research under Dr. Katherine Brown on using CV models to pinpoint and effectively diagnose cardiothoracic diseases based on chest scans.',
-            img: "./public/acm.jpg"
+            img: "./acm.jpg"
           },
         {
             title: 'COINS Lab',
             date: 'May - Dec 2022',
             description: 'Research Assistant for Dr. Richard Golden, where I developed cognitive diagnostic models to assess examination question skills via an intelligent GUI for skill assignment and interrelation analysis.',
-            img: "./public/coins.png"
+            img: "./coins.png"
           },
           
           {
             title: 'Codeucate',
             date: 'Summer 2020',
             description: 'Developed and taught beginner to intermediate Java, JavaScript, Python, and Scratch courses to 100+ students',
-            img: "./public/codeucate.jpg"
+            img: "./codeucate.jpg"
           },
       ];
 
